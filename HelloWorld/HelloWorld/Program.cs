bfc5e4a974ea00ae57a12d5b961b48f0e8effc71
eelian1 <eelizan1@student.gsu.edu>
2017-12-08 11:57:10 -0500

    Added new file

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // using place holders 
+            string name = "Enrico";
+            string msg = string.Format("Hello, {0}", name);
+            Console.WriteLine(msg);
+
+            Console.WriteLine("The number is {0:F3}", 123.45678);
+
+            decimal balance = 1234.5M;
+            Console.WriteLine("The balance is: {0:C}", balance);
+            Console.Read(); 
+        } 
+    }
+}
