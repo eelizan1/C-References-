5e1dac9e7a6a6a546921f6e83a61154e5f87fd92
eelian1 <eelizan1@student.gsu.edu>
2017-12-10 19:00:30 -0500

    Added project

---
+++ Chapter5/Chapter5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Chapter5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Converting a string to a number with user input 
+
+            string userName = "";
+            int userAge = 0;
+            int currentYear = 0;
+
+            Console.Write("Please enter you name: ");
+            userName = Console.ReadLine();
+            Console.Write("Please enter your age: ");
+            userAge = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter the year: ");
+            currentYear = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Name: " + userName);
+            Console.WriteLine("Name: " + userAge);
+            Console.WriteLine("Name: " + currentYear);
+
+            Console.Read(); 
+        }
+    }
+}
